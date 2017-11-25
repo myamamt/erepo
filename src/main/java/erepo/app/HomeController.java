5961c76135f07965696a36d3dcc613e050ad1a4e
Masahiro Yamamoto <m-yamamt@ist.osaka-u.ac.jp>
2017-11-25 17:06:22 +0900

    adjust figure position

--- src/main/java/erepo/app/HomeController.java
+++ src/main/java/erepo/app/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,8 +20,23 @@ public class HomeController {
     public String home(Model model) {
         List<ErrorInfo> recentInfos = errorInfoService.findTop10ByOrderByDateDesc();
         List<CategoryCount> categoryCounts = errorInfoService.findCategoryOrderByCount();
+
+        List<CategoryCount> categoryCountsWithOthers = new ArrayList<>();
+        final int allSum = categoryCounts.stream().mapToInt(c -> c.count).sum();
+        int othersSum = 0;
+        for (CategoryCount categoryCount : categoryCounts) {
+            if (categoryCount.count * 100 >= allSum) {
+                categoryCountsWithOthers.add(categoryCount);
+            } else {
+                othersSum += categoryCount.count;
+            }
+        }
+        if (othersSum > 0) {
+            categoryCountsWithOthers.add(new CategoryCount("others", othersSum));
+        }
+
         model.addAttribute("recentInfos", recentInfos);
-        model.addAttribute("categoryCounts", categoryCounts);
+        model.addAttribute("categoryCounts", categoryCountsWithOthers);
         return "home";
     }
 }
